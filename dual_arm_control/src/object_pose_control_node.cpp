24805234e21202dea547df1390777e87d9064fd2
Marco19987 <marco.desimone@unicampania.it>
2025-01-09 18:36:14 +0100

    modify object_control_node as service server

--- dual_arm_control/src/object_pose_control_node.cpp
+++ dual_arm_control/src/object_pose_control_node.cpp
@@ -1,26 +1,26 @@
-// thid node reads the acutal and the desired object pose and publish the control action (The absolute velocity)
-
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
+#include "std_srvs/srv/set_bool.hpp"
 #include <eigen3/Eigen/Geometry>
 #include "geometry_helper.hpp"
 
 class ObjectPoseControlNode : public rclcpp::Node
 {
 public:
-  ObjectPoseControlNode() : Node("object_pose_control")
+  ObjectPoseControlNode() : Node("object_pose_control"), control_active_(false)
   {
     // declare parameters
     this->declare_parameter("sample_time", 0.02);
     this->get_parameter("sample_time", sample_time_);
 
-    this->declare_parameter("control_gain_diag_vector", std::vector<double>({1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3}));
+    this->declare_parameter("control_gain_diag_vector", std::vector<double>({ 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3 }));
     std::vector<double> control_gain_diag_vector;
     this->get_parameter("control_gain_diag_vector", control_gain_diag_vector);
     control_gain_matrix_.setZero();
-    control_gain_matrix_.diagonal() << control_gain_diag_vector[0], control_gain_diag_vector[1], control_gain_diag_vector[2],
-        control_gain_diag_vector[3], control_gain_diag_vector[4], control_gain_diag_vector[5];
+    control_gain_matrix_.diagonal() << control_gain_diag_vector[0], control_gain_diag_vector[1],
+        control_gain_diag_vector[2], control_gain_diag_vector[3], control_gain_diag_vector[4],
+        control_gain_diag_vector[5];
 
     // declare subscribers and publishers
     sub_object_pose_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
@@ -35,8 +35,10 @@ public:
 
     pub_twist_ = this->create_publisher<geometry_msgs::msg::TwistStamped>("absolute_twist", 1);
 
-    timer_ = this->create_wall_timer(std::chrono::milliseconds((int)(sample_time_ * 1000)),
-                                     std::bind(&ObjectPoseControlNode::timerCallback, this));
+    // create service
+    service_ = this->create_service<std_srvs::srv::SetBool>(
+        "activate_control",
+        std::bind(&ObjectPoseControlNode::activateControlCallback, this, std::placeholders::_1, std::placeholders::_2));
 
     // initialize variables
     bTa_.setIdentity();
@@ -50,17 +52,16 @@ private:
   {
     if (object_pose_read_ && desired_object_pose_read_ && absolute_pose_read_)
     {
-      
       Eigen::Matrix<double, 4, 4> oTa = bTo_.inverse() * bTa_;
       Eigen::Matrix<double, 4, 4> bTa_d = bTo_d_ * oTa;
-      
-      Eigen::Matrix<double, 3,3> aRa_d = bTa_.block<3, 3>(0, 0).transpose() * bTa_d.block<3, 3>(0, 0);
+
+      Eigen::Matrix<double, 3, 3> aRa_d = bTa_.block<3, 3>(0, 0).transpose() * bTa_d.block<3, 3>(0, 0);
       Eigen::AngleAxisd angleAxis(aRa_d);
       Eigen::Vector3d a_error_aa_d = angleAxis.angle() * angleAxis.axis();   // error a-a_d in the absolute frame
-      Eigen::Vector3d b_error_aa_d = bTa_.block<3, 3>(0, 0) * a_error_aa_d; // error a-a_d in the base frame
+      Eigen::Vector3d b_error_aa_d = bTa_.block<3, 3>(0, 0) * a_error_aa_d;  // error a-a_d in the base frame
 
-
-      absolute_twist_.block<3, 1>(0, 0) = control_gain_matrix_.block<3, 3>(0, 0) * (bTa_d.block<3, 1>(0, 3) - bTa_.block<3, 1>(0, 3));
+      absolute_twist_.block<3, 1>(0, 0) =
+          control_gain_matrix_.block<3, 3>(0, 0) * (bTa_d.block<3, 1>(0, 3) - bTa_.block<3, 1>(0, 3));
       absolute_twist_.block<3, 1>(3, 0) = control_gain_matrix_.block<3, 3>(3, 3) * b_error_aa_d;
 
       // publish control
@@ -77,7 +78,7 @@ private:
     }
     else
     {
-      RCLCPP_INFO(this->get_logger(), "Waiting for pose messages...");
+      RCLCPP_INFO(this->get_logger(), "Waiting for pose messages or control activation...");
     }
   }
 
@@ -108,10 +109,41 @@ private:
     uclv::geometry_helper::pose_to_matrix(pose, matrix);
   }
 
+  void activateControlCallback(const std::shared_ptr<std_srvs::srv::SetBool::Request> request,
+                               std::shared_ptr<std_srvs::srv::SetBool::Response> response)
+  {
+    control_active_ = request->data;
+    if (!control_active_)
+    {
+      reset_server();
+    }
+    else
+    {
+      timer_ = this->create_wall_timer(std::chrono::milliseconds((int)(sample_time_ * 1000)),
+                                       std::bind(&ObjectPoseControlNode::timerCallback, this));
+    }
+    response->success = true;
+    response->message = control_active_ ? "Control activated" : "Control deactivated";
+  }
+
+  void reset_server()
+  {
+    timer_->cancel();
+    object_pose_read_ = false;
+    desired_object_pose_read_ = false;
+    absolute_pose_read_ = false;
+    control_active_ = false;
+    bTa_.setIdentity();
+    bTo_.setIdentity();
+    bTo_d_.setIdentity();
+    absolute_twist_.setZero();
+  }
+
   rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr sub_object_pose_;
   rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr sub_desired_object_pose_;
   rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr sub_absolute_pose_;
   rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr pub_twist_;
+  rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr service_;
 
   // transformation matrices
   Eigen::Matrix<double, 4, 4> bTa_;
@@ -133,6 +165,7 @@ private:
   bool object_pose_read_ = false;
   bool desired_object_pose_read_ = false;
   bool absolute_pose_read_ = false;
+  bool control_active_ = false;
 };
 
 int main(int argc, char* argv[])
